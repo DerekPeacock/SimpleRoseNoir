f63d7ae93659cc3ce6a1ccd9846777d916f0c24d
Derek Peacock <derek.peacock@yahoo.com>
2023-02-28 11:23:54 +0000

    started seeding and created database

---
+++ SimpleRoseNoir/Data/DbIntialiser.cs
@@ -0,0 +1,20 @@
+﻿using SimpleRoseNoir.Models;
+
+namespace SimpleRoseNoir.Data
+{
+    public static class DbIntialiser
+    {
+        public static void SeedData(ApplicationDbContext context)
+        {
+            AddProducts(context);
+        }
+
+        private static void AddProducts(ApplicationDbContext context)
+        {
+            var products = new Product[]
+            {
+
+            };
+        }
+    }
+}
---
+++ SimpleRoseNoir/Data/Migrations/20230228111122_IntialCreate.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleRoseNoir.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Variations",
+                columns: table => new
+                {
+                    VariationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Colour = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Variations", x => x.VariationId);
+                    table.ForeignKey(
+                        name: "FK_Variations_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Variations_ProductId",
+                table: "Variations",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Variations");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
