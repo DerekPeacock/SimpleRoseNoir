d2dd6ea2198eae4f6e534da3082b242d8fddf7ad
Derek Peacock <derek.peacock@yahoo.com>
2023-02-28 10:47:06 +0000

    Add project files.

---
+++ SimpleRoseNoir/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleRoseNoir.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [Required, StringLength(60)]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; } = 0;
+    }
+}
