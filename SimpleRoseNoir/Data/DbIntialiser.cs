67bd9df57115d54120e3d47fd1d2d1110964695e
Derek Peacock <derek.peacock@yahoo.com>
2023-03-02 09:51:19 +0000

    Added 2 variations

--- SimpleRoseNoir/Data/DbIntialiser.cs
+++ SimpleRoseNoir/Data/DbIntialiser.cs
@@ -7,13 +7,63 @@ namespace SimpleRoseNoir.Data
         public static void SeedData(ApplicationDbContext context)
         {
             AddProducts(context);
+            AddVariations(context);
         }
+
 
         private static void AddProducts(ApplicationDbContext context)
         {
+            if(context.Products.Any())
+            {
+                return;
+            }
+
             var products = new Product[]
             {
+                new Product()
+                {
+                     Name = "Product 1",
+                     Description = "Product 1 descriptiom ",
+                     Price = 21.00m,
+                },
+                 new Product()
+                {
+                     Name = "Product 2",
+                     Description = "Product 2 descriptiom ",
+                     Price = 22.00m,
+                },
+            };
 
+            foreach ( var product in products)
+            {
+                context.Products.Add(product);
+            }
+
+            context.SaveChanges();
+        }
+        private static void AddVariations(ApplicationDbContext context)
+        {
+            if (context.Variations.Any())
+            {
+                return;
+            }
+
+            var variations = new Variation[]
+            {
+                new Variation()
+                {
+                    Name = "Yellow Product 1",
+                    Description = "Yellow Product 1 Description",
+                    Colour ="Yellow",
+                    Price = 0
+                },
+                new Variation()
+                {
+                    Name = "Blue Product 1",
+                    Description = "Blue Product 1 Description",
+                    Colour ="Blue",
+                    Price = 0
+                }
             };
         }
     }
